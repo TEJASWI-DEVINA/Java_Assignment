8dd99ff3b4a1e016ce6124d9530452ea23ea1f08
TEJASWI-DEVINA <78832726+TEJASWI-DEVINA@users.noreply.github.com>
2022-07-01 10:22:13 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,10 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+/*public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
   }
-}
+}*/
+
+echo "hello all, this is tejaswi"
